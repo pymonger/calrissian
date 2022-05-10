2084e775503870b7d663d4998b9e1d4351f08a20
Gerald Manipon <pymonger@gmail.com>
2022-05-10 13:39:02 -0700

    add script to run calrissian and dump usage

---
+++ run_and_dump_usage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# extract usage report output filename if set
+usage_report=""
+for (( i=1; i <= "$#"; i++ )); do
+    if [ "${!i}" = "--usage-report" ]; then
+      ((i++))
+      usage_report=${!i}
+      break
+    fi
+done
+
+# run calrissian and capture exit status
+calrissian $*
+STATUS=$?
+
+# dump docker usage stats
+if [ ! -z "${usage_report}" ]; then
+  echo "# BEGIN docker-usage.json"
+  cat /calrissian/output-data/${output_pvc_name}/docker-usage.json
+  echo "# END docker-usage.json"
+fi
+
+# propagate exit status of calrissian
+exit $STATUS
